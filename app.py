73df47684341834175727341989cc7c1635cb78e
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-08-26 15:12:50 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,4 +10,13 @@ if st.checkbox("Select me!"):
 import pandas as pd
 st.title("Last 100 IPOs")
 df = pd.read_html('https://www.iposcoop.com/last-100-ipos/')[0]
+
+industries = df['Industry'].unique()
+pickSector = st.multiselect("Pick an industry to filter", industries)
+
 st.dataframe(df)
+filterDF = df[df['Industry'].isin(pickSector)]
+
+st.write("Filtered table")
+st.dataframe(filterDF)
+
