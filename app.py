63ad515a470fc5179a37d90dc303a10f321d8fb0
Bogdan Tudose <59750436+dbogt@users.noreply.github.com>
2022-08-26 15:08:23 -0400

    ipos table

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ st.multiselect("pick an option!", categories)
 st.sidebar.button("Click me!")
 if st.checkbox("Select me!"):
   st.write("you selected the checkbox!")
+
+import pandas as pd
+df = pd.read_html('https://www.iposcoop.com/last-100-ipos/')[0]
+st.dataframe(df)
